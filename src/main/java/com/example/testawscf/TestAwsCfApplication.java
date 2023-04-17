48d8dea1d668c52c60137300a25661bc37b112bc
diypcfev <username@email.com>
2023-04-17 19:45:14 +0800

    Initial commit

---
+++ src/main/java/com/example/testawscf/TestAwsCfApplication.java
@@ -0,0 +1,13 @@
+package com.example.testawscf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAwsCfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAwsCfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testawscf/TestAwsCfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testawscf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAwsCfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
